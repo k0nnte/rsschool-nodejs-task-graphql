25b5350645a644dabbb9cdcde3406c4fb9a23794
k0nnte <bystrov.igor25@gmail.com>
2025-05-26 22:52:44 +0300

    feat: start quety

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,6 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLSchema } from 'graphql';
+import { rootQuety } from './types/query.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +20,8 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+
+export const shema = new GraphQLSchema({
+  query: rootQuety,
+})
---
+++ src/routes/graphql/types/user.ts
@@ -0,0 +1,18 @@
+import { GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { UUIDType } from './uuid.js'
+
+
+export const User = new GraphQLObjectType({
+    name: "User", 
+    fields: () => ({
+        id: {type: new GraphQLNonNull(UUIDType)},
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        balance: {type: new GraphQLNonNull(GraphQLFloat)},
+        profile: {
+            type: "Profile",
+            resolve: async (parent, args, contesxt) => {
+                
+            }
+        }
+    })
+})
